c052f4dff7919fb2baa8288a4b2d4861753d5be2
JLA <arancibiajav@gmail.com>
2014-05-21 18:05:39 -0300

    Ledor Vador ASMX Test Unit added

--- ClarityServicesTestUnits/Program.cs
+++ ClarityServicesTestUnits/Program.cs
@@ -4,7 +4,8 @@ using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ClarityServicesTestUnits.LVFWS;
+//using ClarityServicesTestUnits.LVFWS;
+using ClarityServicesTestUnits.LVASMX_SMG01;
 
 
 
@@ -21,11 +22,50 @@ namespace ClarityServicesTestUnits {
         }
 
         public static void LedorVadorServiceASMXTestUnit() {
+
+
+
+            List<LedorVadorFlowPackage> packages = new List<LedorVadorFlowPackage>();
+
+            packages.Add(new LedorVadorFlowPackage() {
+                histClinica = Convert.ToInt32(ConfigurationManager.AppSettings.Get("HistClinica")),
+                razonSocial = ConfigurationManager.AppSettings.Get("RazonSocial"),
+                empresaPropia = Convert.ToInt32(ConfigurationManager.AppSettings.Get("EmpresaID_Propia")),
+                documento_externId = ConfigurationManager.AppSettings.Get("Documento_ExternID"),
+                establecimiento_numero = ConfigurationManager.AppSettings.Get("Establecimiento_Numero"),
+                documento_numero = ConfigurationManager.AppSettings.Get("Documento_Numero"),
+                fecha = DateTime.Now,
+                observacion = ConfigurationManager.AppSettings.Get("Observacion"),
+                tipoServicioId = Convert.ToInt32(ConfigurationManager.AppSettings.Get("TipoServicioID")),
+                tipoImpuestoId_Iva = Convert.ToInt32(ConfigurationManager.AppSettings.Get("TipoImpuestoID_IVA")),
+                cantidad = Convert.ToDouble(ConfigurationManager.AppSettings.Get("Cantidad")),
+                importeUnitario = Convert.ToDouble(ConfigurationManager.AppSettings.Get("ImporteUnitario"))
+            });
+
+            LedorVadorRequest request = new LedorVadorRequest() {
+                login = ConfigurationManager.AppSettings.Get("login"),
+                password = ConfigurationManager.AppSettings.Get("pass"),
+                packages = packages.ToArray()
+            };
+
+            SendPackagesRequest req = new SendPackagesRequest() {
+                Body = new SendPackagesRequestBody(request)
+            };
+
+            try {
+                LVASMX_SMG01.LVServiceSoapClient client = new LVServiceSoapClient();
+                SendPackagesResponse resp = client.SendPackages(req);
+                LedorVadorResponse response = resp.Body.SendPackagesResult;
+                Console.WriteLine(response.responseMessage);
+            } catch (Exception e) {
+                Console.WriteLine("[LedorVadorFlowService TestUnit Exception][Posible EnpointAdress nulo][" + e.ToString().Substring(0, 300) + "]");
+            }
+
             Console.ReadKey();
         }
 
         public static void LedorVadorFlowServiceTestUnit() {
-            
+            /*
             LedorVadorFlowServiceClient service = new LedorVadorFlowServiceClient();
 
             //LedorVadorFlowService localService = new LedorVadorFlowService();
@@ -66,6 +106,7 @@ namespace ClarityServicesTestUnits {
             } catch (Exception e) {
                 Console.WriteLine("[LedorVadorFlowService TestUnit Exception][Posible EnpointAdress nulo][" + e.ToString().Substring(0, 300) + "]");
             }
+             * */
             Console.ReadKey();
         }
         //
